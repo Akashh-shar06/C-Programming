80a867ed4891ca478eef62ceb62759d1617cc0a9
Aakash Sharma <akashshar9029@gmail.com>
2025-08-06 19:15:19 +0530

    Create Total sum in While Loop in.c

---
+++ Total sum in While Loop in.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int i = 1 , num , sum =0;
+    
+    printf("Enter the number:");
+    scanf("%d" , &num);
+    
+    while (i <= num){
+        sum = sum + i;
+        i++;
+    }
+         printf("the total sum is:%d\n" , sum);
+
+    return 0;
+}
