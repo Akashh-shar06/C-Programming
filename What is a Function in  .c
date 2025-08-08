003d13cac340dace1e77c78fa68ff47cbb306486
Aakash Sharma <akashshar9029@gmail.com>
2025-08-08 19:09:12 +0530

    Create What is a Function in  .c

---
+++ What is a Function in  .c
@@ -0,0 +1,122 @@
+What is a Function in C?
+In C programming, a function is a block of code that performs a specific task. Instead of writing the same code again and again, we can reuse the function wherever needed.
+
+Why Do We Use Functions in C?
+
+Code Reusability – Write once, use many times.
+Modularity – Split big programs into small modules.
+Ease of Testing and Debugging – Each function can be tested separately.
+Improved Readability and Maintenance – Organized structure.
+
+Types of Functions in C
+Functions in C can be categorized as:
+
+1 - Library Functions
+Built-in functions provided by C (e.g., printf(), scanf(), sqrt()).
+No need to define them.
+
+2 - User-defined Functions
+Created by the programmer.
+Syntax: return_type function_name(parameters) { /* code */ }
+
+---------------------- Addition User Define Function Types ------------------------------
+         1. Without Return & Without Parameters
+         2. With Return & Without Parameters
+         3. With Parameters & Without Return
+         4. With Parameters & With Return
+
+---------------------------------------------------------------------------------------
+1. Without Return Value & Without Parameters
+PROGRAM:---------------------
+
+#include <stdio.h>
+
+void add() { -- Parameter
+    int a = 5, b = 3;
+    int sum = a + b;
+    printf("Sum = %d\n", sum);
+}
+
+int main() {
+    add();  // function call Argument
+    return 0;
+}
+
+**code**
+// Online C compiler to run C program online
+#include <stdio.h>
+
+void Addition(){
+    int num1 , num2;
+    printf("enter the value:");
+    scanf("%d" , &num1);
+    
+    printf("enter the value:");
+    scanf("%d" , &num2);
+    
+    printf("%d + %d = %d\n" , num1 , num2 , num1+num2);
+}
+
+void substraction() {
+    int num1 , num2;
+    printf("enter the value:");
+    scanf("%d" , &num1);
+    
+    printf("enter the value:");
+    scanf("%d" , &num2);
+    
+    printf("%d - %d = %d\n" , num1 , num2 , num1-num2);
+}
+
+void Multiplication(){
+    int num1 , num2;
+    printf("enter the value:");
+    scanf("%d" , &num1);
+    
+    printf("enter the value:");
+    scanf("%d" , &num2);
+    
+    printf("%d * %d = %d\n" , num1 , num2 , num1*num2);
+}
+
+void Division(){
+    int num1 , num2;
+    printf("enter the value:");
+    scanf("%d" , &num1);
+    
+    printf("enter the value:");
+    scanf("%d" , &num2);
+    
+    printf("%d / %d = %d\n" , num1 , num2 , num1/num2);
+}
+
+int main(){
+    int choice;
+    
+    printf("\n enter the choice:\n");
+    printf("1. Addtition\n");
+    printf("2. Substration\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    scanf("%d" , &choice);
+    
+    switch(choice){
+        case 1:
+            Addition();
+            break;    
+        case 2:
+            substraction();
+            break;      
+        case 3:
+            Multiplication();
+            break;
+        case 4:
+            Division();
+            break;
+        default:
+            printf("Invalid choice.\n");
+    }
+
+    return 0;
+}
+
